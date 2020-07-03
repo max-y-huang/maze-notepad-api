fc05d3b4f80a06d613a8ea4a72b317cea4a71c4d
Max Huang <letmaxknow@gmail.com>
2020-07-03 16:23:21 -0400

    added filter for maze get

--- routes/maze/get.js
+++ routes/maze/get.js
@@ -7,8 +7,17 @@ router.get('/', async (req, res, next) => {
 
   let { db, dbo } = await mdb.getDb().catch(err => res.status(500).json({ 'result': err }));
 
+  let query = {};
+  if (req.query.tags) {
+    // Searching for keywords.
+    // Rule: all of the keywords must be at least partially contained in the tags.
+    query.$and = req.query.tags.split(',').map(tag => {  // All elements in query.$and must be true to satisfy the condition.
+      return { 'tags': new RegExp(`.*${tag.trim()}.*`, 'i') };  // Regex searches if string contains tag.trim().
+    });
+  };
+
   let found = await new Promise((resolve, reject) => {
-    dbo.collection('mazes').find({}).toArray((err, res) => {
+    dbo.collection('mazes').find(query).toArray((err, res) => {
       if (err) {
         reject(err);
       };
