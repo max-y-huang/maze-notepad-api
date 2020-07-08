7193577016327026d6c3445cb36ff693b9a48e0e
Max Huang <letmaxknow@gmail.com>
2020-07-07 22:42:23 -0400

    Comments

--- routes/maze/get.js
+++ routes/maze/get.js
@@ -4,13 +4,16 @@ const router = express.Router();
 const mdb = require('./../../utils/mongo');
 const funcs = require('./../../utils/funcs');
 
+// Returns mazes (offset and limited like pages) that fulfill the input filter. Also returns the total count (that fulfills the filter).
 router.get('/', async (req, res, next) => {
 
   let defaultPage = 1;
   let defaultPageSize = 20;
 
+  // Get database.
   let { db, dbo } = await mdb.getDb().catch(err => res.status(500).json({ 'result': err }));
 
+  // Setup the search query with the filter information.
   let query = {};
   if (req.query.tags) {
     // Searching for keywords.
@@ -19,12 +22,16 @@ router.get('/', async (req, res, next) => {
       return { '__tag-names': new RegExp(`.*${tag}.*`, 'i') };  // Regex searches if string contains tag.
     });
   };
+  // Setup the page and page size.
   let page = req.query.page ? parseInt(req.query.page) : defaultPage;
   let pageSize = req.query['page-size'] ? parseInt(req.query['page-size']) : defaultPageSize;
 
+  // Get the total count.
   let count = await dbo.collection('mazes').find(query, {}).count();
+  // Return the found items.
+  let skipCount = (page - 1) * pageSize;
   let items = await new Promise((resolve, reject) => {
-    dbo.collection('mazes').find(query, { projection: { '__tag-names': 0 } }).skip((page - 1) * pageSize).limit(pageSize).toArray((err, res) => {
+    dbo.collection('mazes').find(query, { projection: { '__tag-names': 0 } }).skip(skipCount).limit(pageSize).toArray((err, res) => {
       if (err) {
         reject(err);
       };
@@ -33,6 +40,7 @@ router.get('/', async (req, res, next) => {
     });
   });
 
+  // Success.
   return res.status(200).json({ 'result': { 'total-count': count, 'items': items } });
 });
 
--- routes/maze/upload.js
+++ routes/maze/upload.js
@@ -8,13 +8,18 @@ const mdb = require('./../../utils/mongo');
 const funcs = require('./../../utils/funcs');
 const upload = multer({ dest: './uploads' });
 
+// Upload fields for POST request.
 const uploadFields = [
   { name: 'maze-file', maxCount: 1 },
   { name: 'image-file', maxCount: 1 }
 ];
 
+// Uploads a maze to the MongoDB database, uploads required files to the AWS S3 Bucket, returns the MongoDB id of the newly created maze.
 router.post('/', upload.fields(uploadFields), async (req, res, next) => {
 
+  // TODO: Use proper file extension code.
+
+  // Validate fields.
   let missingParams = [];
   if (!req.files) {
     return res.status(500).json({ 'result': 'Missing req.files and/or req.body.' });
@@ -35,6 +40,7 @@ router.post('/', upload.fields(uploadFields), async (req, res, next) => {
     return res.status(400).json({ 'result': `Missing the following required parameters: ${missingParams.join(', ')}` });
   }
 
+  // Convert input from strings/files to workable data.
   let input__name = req.body.name;
   let input__mazeFileName = req.files['maze-file'][0].filename;
   let input__imageFileName = req.files['image-file'][0].filename;
@@ -44,19 +50,21 @@ router.post('/', upload.fields(uploadFields), async (req, res, next) => {
   ], 'name');
   let input__tagNames = input__tags.map(x => x.name);
 
+  // Get database.
   let { db, dbo } = await mdb.getDb().catch(err => res.status(500).json({ 'result': err }));
 
+  // Get the id of the inserted element. Returned in success.
   let insertedId = await new Promise((resolve, reject) => {
 
-    dbo.collection('mazes').insertOne({
-
+    // Insert a maze into the mazes collection.
+    let objectForInsertion = {
       'name': input__name,
       'maze-file-name': `${input__mazeFileName}.mznp`,
       'image-file-name': `${input__imageFileName}.png`,
       'tags': input__tags,
       '__tag-names': input__tagNames
-      
-    }, (err, res) => {
+    };
+    dbo.collection('mazes').insertOne(objectForInsertion, (err, res) => {
       if (err) {
         reject(err);
       };
@@ -64,11 +72,11 @@ router.post('/', upload.fields(uploadFields), async (req, res, next) => {
 
       aws.getS3((s3) => {
 
+        // Retrieve newly uploaded files (directory set with Multer).
         let mazeFile = fs.readFileSync(`uploads/${input__mazeFileName}`);
         let imageFile = fs.readFileSync(`uploads/${input__imageFileName}`);
         
-        // TODO: Use proper file extension code.
-        // Upload maze and image to AWS S3.
+        // Upload maze and image to the AWS S3 bucket.
         s3.upload({ Bucket: 'maze-notepad', Key: `mazes/${input__mazeFileName}.mznp`, Body: mazeFile }, (err, data) => {
           if (err) {
             reject(err); 
@@ -84,6 +92,7 @@ router.post('/', upload.fields(uploadFields), async (req, res, next) => {
     });
   });
 
+  // Success.
   return res.status(200).json({ 'result': { 'id': insertedId } });
 });
 
